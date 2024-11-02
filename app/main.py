f87793574e4fc5a8387432d97861ce1f1d229627
Suhas Herle <herlesuhas@gmail.com>
2024-11-02 09:27:20 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,23 @@
 import socket  # noqa: F401
 
+def parse_request_lines(http_request):
+    http_request_line = http_request.split('\r\n')[0]
+    http_method, request_target, _ = http_request_line.split(' ', 2)
+    return http_method, request_target
+
+def parse_headers(http_request):
+    headers = {}
+    lines = http_request.split('\r\n')
+    for line in lines[1:]:
+        if ': ' in line:
+            key,value = line.split(': ')
+            headers[key] = value
+    
+    host = headers.get('Host')
+    user_agent = headers.get('User-Agent')
+    accept = headers.get('Accept')
+       
+    return host, user_agent, accept
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,7 +27,19 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, addr = server_socket.accept()
-    msg = 'HTTP/1.1 200 OK\r\n\r\n'
+    http_request = client_socket.recv(4096).decode()
+    
+    http_method, request_target = parse_request_lines(http_request)
+    host, user_agent, accept = parse_headers(http_request)
+    
+    msg = None
+    
+    if request_target == '/' or request_target is None:
+        msg = 'HTTP/1.1 200 OK\r\n\r\n'
+    
+    else:
+        msg = 'HTTP/1.1 404 Not Found\r\n\r\n'    
+   
     encoded_string = msg.encode()
     client_socket.sendall(encoded_string)
     client_socket.close()
